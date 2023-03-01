67be8d65c11c2444c3d8aaddead71f553339ccc3
EC2 Default User <ec2-user@ip-172-31-10-103.ap-northeast-1.compute.internal>
2023-03-01 06:35:29 +0000

    感情点数機能完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_19_074613) do
+ActiveRecord::Schema.define(version: 2023_03_01_062411) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2023_02_19_074613) do
     t.string "detail", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "customers", force: :cascade do |t|
